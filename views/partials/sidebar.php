027ab5a3e0f3ec7a90bf32e5e73e3c3494ce28d4
jesuszapataDev <jesusnbz22@gmail.com>
2025-10-02 10:40:39 -0400

    update sidebar

--- views/partials/sidebar.php
+++ views/partials/sidebar.php
@@ -107,19 +107,19 @@ function renderMenuItems($items, $level = 1)
 
     <a href="index.html" class="logo text-center logo-light">
         <span class="logo-lg">
-            <img src="assets/images/logo.png" alt="" height="16">
+            <img src="<?= BASE_URL ?>public/assets/images/logo.png" alt="" height="16">
         </span>
         <span class="logo-sm">
-            <img src="assets/images/logo_sm.png" alt="" height="16">
+            <img src="<?= BASE_URL ?>public/assets/images/logo_sm.png" alt="" height="16">
         </span>
     </a>
 
     <a href="index.html" class="logo text-center logo-dark">
         <span class="logo-lg">
-            <img src="assets/images/logo-dark.png" alt="" height="16">
+            <img src="<?= BASE_URL ?>public/assets/images/logo-dark.png" alt="" height="16">
         </span>
         <span class="logo-sm">
-            <img src="assets/images/logo_sm_dark.png" alt="" height="16">
+            <img src="<?= BASE_URL ?>public/assets/images/logo_sm_dark.png" alt="" height="16">
         </span>
     </a>
 
