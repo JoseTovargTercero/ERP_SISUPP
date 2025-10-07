9f3da372b36849253c6a1bec0c46e61107241c70
moisescelis0702 <moisescelis21@gmail.com>
2025-10-07 11:48:10 -0400

    Db actualizada

--- sql/erp_g.sql
+++ sql/erp_g.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 06-10-2025 a las 01:30:30
+-- Tiempo de generación: 07-10-2025 a las 17:47:46
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -71,7 +71,8 @@ CREATE TABLE `animales` (
 --
 
 INSERT INTO `animales` (`animal_id`, `identificador`, `sexo`, `especie`, `raza`, `color`, `fecha_nacimiento`, `estado`, `etapa_productiva`, `categoria`, `origen`, `madre_id`, `padre_id`, `fotografia_url`, `created_at`, `created_by`, `updated_at`, `updated_by`, `deleted_at`, `deleted_by`) VALUES
-('9e9394fe-00ac-47ef-a3b7-e97bd3ac0c63', 'TEST-7496RV', 'MACHO', 'BOVINO', 'Criollo', 'Negro y Blanco', '2025-10-05', 'ACTIVO', 'CEBA', 'ENGORDE', 'OTRO', NULL, NULL, NULL, '2025-10-05 12:12:02', 'd7518474-2d2f-4634-823f-71936565c110', '2025-10-05 12:12:02', 'd7518474-2d2f-4634-823f-71936565c110', NULL, NULL);
+('9e9394fe-00ac-47ef-a3b7-e97bd3ac0c63', 'TEST-7496RV', 'MACHO', 'BOVINO', 'Criollo', 'Negro y Blanco', '2025-10-05', 'ACTIVO', 'CEBA', 'ENGORDE', 'NACIMIENTO', NULL, NULL, '/uploads/9e9394fe-00ac-47ef-a3b7-e97bd3ac0c63.png', '2025-10-05 12:12:02', 'd7518474-2d2f-4634-823f-71936565c110', '2025-10-05 20:04:00', '202b02fa-053d-48d5-a307-b52adb5525f4', NULL, NULL),
+('ab636534-891c-43fb-b501-56177e79f621', 'TEST-7496RV2', 'HEMBRA', 'BOVINO', 'Criollo', NULL, '2025-10-07', 'ACTIVO', NULL, NULL, 'NACIMIENTO', NULL, NULL, '/uploads/ab636534-891c-43fb-b501-56177e79f621.png', '2025-10-07 10:14:15', 'd7518474-2d2f-4634-823f-71936565c110', '2025-10-07 10:14:15', 'd7518474-2d2f-4634-823f-71936565c110', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -103,13 +104,6 @@ CREATE TABLE `animal_movimientos` (
   `deleted_by` char(36) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Volcado de datos para la tabla `animal_movimientos`
---
-
-INSERT INTO `animal_movimientos` (`animal_movimiento_id`, `animal_id`, `fecha_mov`, `tipo_movimiento`, `motivo`, `estado`, `finca_origen_id`, `aprisco_origen_id`, `area_origen_id`, `finca_destino_id`, `aprisco_destino_id`, `area_destino_id`, `costo`, `documento_ref`, `observaciones`, `created_at`, `created_by`, `updated_at`, `updated_by`, `deleted_at`, `deleted_by`) VALUES
-('1f56c053-ebd7-4a75-b068-c97a531a93c3', '9e9394fe-00ac-47ef-a3b7-e97bd3ac0c63', '2025-10-05', 'OTRO', 'OTRO', 'ANULADO', NULL, NULL, NULL, NULL, NULL, NULL, 123.45, 'DOC-PRUEBA-001', 'Movimiento de prueba', '2025-10-05 12:12:03', 'd7518474-2d2f-4634-823f-71936565c110', '2025-10-05 12:12:03', 'd7518474-2d2f-4634-823f-71936565c110', NULL, NULL);
-
 -- --------------------------------------------------------
 
 --
@@ -136,7 +130,8 @@ CREATE TABLE `animal_pesos` (
 --
 
 INSERT INTO `animal_pesos` (`animal_peso_id`, `animal_id`, `fecha_peso`, `peso_kg`, `metodo`, `observaciones`, `created_at`, `created_by`, `updated_at`, `updated_by`, `deleted_at`, `deleted_by`) VALUES
-('e785f5ab-3f55-4b0e-aaaa-b7b7f3115bdc', '9e9394fe-00ac-47ef-a3b7-e97bd3ac0c63', '2025-10-05', 255.300, 'BALANZA', 'Ajuste de peso (recalibración)', '2025-10-05 12:12:03', 'd7518474-2d2f-4634-823f-71936565c110', '2025-10-05 12:12:03', 'd7518474-2d2f-4634-823f-71936565c110', NULL, NULL);
+('e785f5ab-3f55-4b0e-aaaa-b7b7f3115bdc', '9e9394fe-00ac-47ef-a3b7-e97bd3ac0c63', '2025-10-05', 255.300, 'BALANZA', 'Ajuste de peso (recalibración)', '2025-10-05 12:12:03', 'd7518474-2d2f-4634-823f-71936565c110', '2025-10-05 12:12:03', 'd7518474-2d2f-4634-823f-71936565c110', NULL, NULL),
+('ff91ed4a-4d10-4992-8a77-b6628439f171', '9e9394fe-00ac-47ef-a3b7-e97bd3ac0c63', '2025-10-06', 10.000, 'balanza', '', '2025-10-05 19:50:47', 'd7518474-2d2f-4634-823f-71936565c110', NULL, NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -173,6 +168,7 @@ CREATE TABLE `animal_salud` (
 --
 
 INSERT INTO `animal_salud` (`animal_salud_id`, `animal_id`, `fecha_evento`, `tipo_evento`, `diagnostico`, `severidad`, `tratamiento`, `medicamento`, `dosis`, `via_administracion`, `costo`, `estado`, `proxima_revision`, `responsable`, `observaciones`, `created_at`, `created_by`, `updated_at`, `updated_by`, `deleted_at`, `deleted_by`) VALUES
+('142a7d3f-f439-4178-8f05-8f26ba3de22e', '9e9394fe-00ac-47ef-a3b7-e97bd3ac0c63', '2025-10-05', 'ENFERMEDAD', 'fsafa', 'NO_APLICA', 'fsafa', 'fsafa', 'fsa', 'fsa', 25.00, 'ABIERTO', '2025-10-05', 'fsa', 'fsa', '2025-10-05 19:51:23', 'd7518474-2d2f-4634-823f-71936565c110', NULL, NULL, NULL, NULL),
 ('c041f6e1-0509-4b29-b587-f387f3dddea2', '9e9394fe-00ac-47ef-a3b7-e97bd3ac0c63', '2025-10-05', 'REVISION', 'Revisión general', 'LEVE', 'N/A', NULL, NULL, NULL, NULL, 'CERRADO', NULL, 'Encargado 1', 'Caso cerrado por estabilidad', '2025-10-05 12:12:03', 'd7518474-2d2f-4634-823f-71936565c110', '2025-10-05 12:12:03', 'd7518474-2d2f-4634-823f-71936565c110', NULL, NULL);
 
 -- --------------------------------------------------------
@@ -205,7 +201,9 @@ CREATE TABLE `animal_ubicaciones` (
 --
 
 INSERT INTO `animal_ubicaciones` (`animal_ubicacion_id`, `animal_id`, `finca_id`, `aprisco_id`, `area_id`, `fecha_desde`, `fecha_hasta`, `motivo`, `estado`, `observaciones`, `created_at`, `created_by`, `updated_at`, `updated_by`, `deleted_at`, `deleted_by`) VALUES
-('cbd8136b-315f-4e40-9429-541d25e8f0d3', '9e9394fe-00ac-47ef-a3b7-e97bd3ac0c63', '06fcbfc8-ffc7-4956-b99d-77d879d772b7', '78059699-0f15-419e-89a8-fcc2697c4c97', '9927c9e7-d35a-4b1c-93b0-c078894cc9ef', '2025-10-05', '2025-10-05', 'INGRESO', 'INACTIVA', 'Observación editada', '2025-10-05 12:12:03', 'd7518474-2d2f-4634-823f-71936565c110', '2025-10-05 12:12:03', 'd7518474-2d2f-4634-823f-71936565c110', NULL, NULL);
+('1db3d6d8-b8eb-4895-9c20-47919881fa56', '9e9394fe-00ac-47ef-a3b7-e97bd3ac0c63', '06fcbfc8-ffc7-4956-b99d-77d879d772b7', '78059699-0f15-419e-89a8-fcc2697c4c97', '9927c9e7-d35a-4b1c-93b0-c078894cc9ef', '2025-10-06', '2025-10-31', 'TRASLADO', 'INACTIVA', '', '2025-10-05 20:19:04', '202b02fa-053d-48d5-a307-b52adb5525f4', NULL, NULL, NULL, NULL),
+('41b65188-9e88-442e-baca-be16d85b712b', '9e9394fe-00ac-47ef-a3b7-e97bd3ac0c63', '06fcbfc8-ffc7-4956-b99d-77d879d772b7', '78059699-0f15-419e-89a8-fcc2697c4c97', '9927c9e7-d35a-4b1c-93b0-c078894cc9ef', '2025-10-06', '2025-10-31', 'INGRESO', 'ACTIVA', 'fsafa', '2025-10-05 20:17:45', '202b02fa-053d-48d5-a307-b52adb5525f4', NULL, NULL, NULL, NULL),
+('5692f375-158f-4048-b465-6d3259a7a6d1', '9e9394fe-00ac-47ef-a3b7-e97bd3ac0c63', '06fcbfc8-ffc7-4956-b99d-77d879d772b7', '78059699-0f15-419e-89a8-fcc2697c4c97', '9927c9e7-d35a-4b1c-93b0-c078894cc9ef', '2025-10-06', NULL, 'TRASLADO', 'ACTIVA', 'fsafa', '2025-10-05 20:21:20', '202b02fa-053d-48d5-a307-b52adb5525f4', NULL, NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -329,8 +327,21 @@ CREATE TABLE `montas` (
   `monta_id` char(36) NOT NULL,
   `periodo_id` char(36) NOT NULL,
   `numero_monta` tinyint(3) UNSIGNED NOT NULL,
-  `fecha_monta` datetime NOT NULL
+  `fecha_monta` datetime NOT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `created_by` char(36) DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `updated_by` char(36) DEFAULT NULL,
+  `deleted_at` datetime DEFAULT NULL,
+  `deleted_by` char(36) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `montas`
+--
+
+INSERT INTO `montas` (`monta_id`, `periodo_id`, `numero_monta`, `fecha_monta`, `created_at`, `created_by`, `updated_at`, `updated_by`, `deleted_at`, `deleted_by`) VALUES
+('4165a60c-712a-49ca-8d51-2f58d1189179', '2efa8a7b-4fce-40c9-b71a-0e6af178df9f', 6, '2025-10-07 00:00:00', '2025-10-07 10:48:36', 'd7518474-2d2f-4634-823f-71936565c110', '2025-10-07 10:48:36', 'd7518474-2d2f-4634-823f-71936565c110', '2025-10-07 10:48:36', 'd7518474-2d2f-4634-823f-71936565c110');
 
 -- --------------------------------------------------------
 
@@ -346,8 +357,21 @@ CREATE TABLE `partos` (
   `crias_hembras` smallint(5) UNSIGNED NOT NULL DEFAULT 0,
   `peso_promedio_kg` decimal(5,2) DEFAULT NULL,
   `estado_parto` enum('NORMAL','DISTOCIA','MUERTE_PERINATAL','OTRO') NOT NULL DEFAULT 'NORMAL',
-  `observaciones` varchar(255) DEFAULT NULL
+  `observaciones` varchar(255) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `created_by` char(36) DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `updated_by` char(36) DEFAULT NULL,
+  `deleted_at` datetime DEFAULT NULL,
+  `deleted_by` char(36) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `partos`
+--
+
+INSERT INTO `partos` (`parto_id`, `periodo_id`, `fecha_parto`, `crias_machos`, `crias_hembras`, `peso_promedio_kg`, `estado_parto`, `observaciones`, `created_at`, `created_by`, `updated_at`, `updated_by`, `deleted_at`, `deleted_by`) VALUES
+('2adba12c-538a-4452-b907-ffe9d2c72f8e', '2efa8a7b-4fce-40c9-b71a-0e6af178df9f', '2025-10-07', 1, 1, 3.60, 'DISTOCIA', 'Parto actualizado (test)', '2025-10-07 10:48:36', 'd7518474-2d2f-4634-823f-71936565c110', '2025-10-07 10:48:36', 'd7518474-2d2f-4634-823f-71936565c110', '2025-10-07 10:48:36', 'd7518474-2d2f-4634-823f-71936565c110');
 
 -- --------------------------------------------------------
 
@@ -361,7 +385,41 @@ CREATE TABLE `periodos_servicio` (
   `verraco_id` char(36) NOT NULL,
   `fecha_inicio` date NOT NULL,
   `observaciones` varchar(255) DEFAULT NULL,
-  `estado_periodo` enum('ABIERTO','CERRADO') NOT NULL DEFAULT 'ABIERTO'
+  `estado_periodo` enum('ABIERTO','CERRADO') NOT NULL DEFAULT 'ABIERTO',
+  `created_at` datetime DEFAULT NULL,
+  `created_by` char(36) DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `updated_by` char(36) DEFAULT NULL,
+  `deleted_at` datetime DEFAULT NULL,
+  `deleted_by` char(36) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `periodos_servicio`
+--
+
+INSERT INTO `periodos_servicio` (`periodo_id`, `hembra_id`, `verraco_id`, `fecha_inicio`, `observaciones`, `estado_periodo`, `created_at`, `created_by`, `updated_at`, `updated_by`, `deleted_at`, `deleted_by`) VALUES
+('2efa8a7b-4fce-40c9-b71a-0e6af178df9f', 'ab636534-891c-43fb-b501-56177e79f621', '9e9394fe-00ac-47ef-a3b7-e97bd3ac0c63', '2025-10-07', 'Observación actualizada (test)', 'CERRADO', '2025-10-07 10:48:36', 'd7518474-2d2f-4634-823f-71936565c110', '2025-10-07 10:48:36', 'd7518474-2d2f-4634-823f-71936565c110', '2025-10-07 10:48:36', 'd7518474-2d2f-4634-823f-71936565c110');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `recintos`
+--
+
+CREATE TABLE `recintos` (
+  `recinto_id` char(36) NOT NULL,
+  `area_id` char(36) NOT NULL,
+  `codigo_recinto` varchar(50) NOT NULL,
+  `capacidad` smallint(5) UNSIGNED DEFAULT NULL,
+  `estado` enum('ACTIVO','INACTIVO') NOT NULL DEFAULT 'ACTIVO',
+  `observaciones` text DEFAULT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `created_by` char(36) DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `updated_by` char(36) DEFAULT NULL,
+  `deleted_at` datetime DEFAULT NULL,
+  `deleted_by` char(36) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -396,7 +454,7 @@ CREATE TABLE `reportes_dano` (
 --
 
 INSERT INTO `reportes_dano` (`reporte_id`, `finca_id`, `aprisco_id`, `area_id`, `titulo`, `descripcion`, `criticidad`, `estado_reporte`, `fecha_reporte`, `reportado_por`, `solucionado_por`, `fecha_cierre`, `created_at`, `created_by`, `updated_at`, `updated_by`, `deleted_at`, `deleted_by`) VALUES
-('def09c3d-e0e5-48a3-b53a-8953f724a6d9', '06fcbfc8-ffc7-4956-b99d-77d879d772b7', '78059699-0f15-419e-89a8-fcc2697c4c97', '9927c9e7-d35a-4b1c-93b0-c078894cc9ef', 'Daño en bebedero rd20er', 'Fuga intermitente; requiere cambio de manguera.', 'ALTA', 'CERRADO', '2025-10-02 10:52:17', NULL, NULL, '2025-10-02 10:52:17', '2025-10-02 10:52:17', 'def09c3d-e0e5-48a3-b53a-8953f724a6d9', '2025-10-02 10:52:17', 'def09c3d-e0e5-48a3-b53a-8953f724a6d9', NULL, NULL);
+('def09c3d-e0e5-48a3-b53a-8953f724a6d9', '06fcbfc8-ffc7-4956-b99d-77d879d772b7', '78059699-0f15-419e-89a8-fcc2697c4c97', '9927c9e7-d35a-4b1c-93b0-c078894cc9ef', 'Daño en bebedero rd20er', 'Fuga intermitente; requiere cambio de manguera.', 'ALTA', 'EN_PROCESO', '2025-10-02 10:52:17', NULL, NULL, '2025-10-02 10:52:17', '2025-10-02 10:52:17', 'def09c3d-e0e5-48a3-b53a-8953f724a6d9', '2025-10-05 19:52:21', 'd7518474-2d2f-4634-823f-71936565c110', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -442,6 +500,7 @@ CREATE TABLE `system_users` (
 INSERT INTO `system_users` (`user_id`, `nombre`, `email`, `contrasena`, `nivel`, `estado`, `created_at`, `created_by`, `updated_at`, `updated_by`, `deleted_at`, `deleted_by`) VALUES
 ('07d9bff2-7493-4695-92e0-1ca74a48db06', 'Luis Aguirrin', 'aguirrin@gmail.com', '$2y$10$GiHdtU89HFwKXHVYwWFmGeZzLIWjFjpwIYm0OyHGazFJyQmh145ky', 1, 1, '2025-10-05 16:08:11', 'd7518474-2d2f-4634-823f-71936565c110', NULL, NULL, NULL, NULL),
 ('1', 'Fatima Gomez', 'fatimagomezpd@gmail.com', '$2y$10$EyP1MOY39kuw4uREdk7ao.UUzQ10YNIZ95IZLM70MUPo5J6YzEBVG', 1, 1, NULL, NULL, NULL, NULL, '2025-10-05 16:14:00', 'd7518474-2d2f-4634-823f-71936565c110'),
+('202b02fa-053d-48d5-a307-b52adb5525f4', 'Moises', 'moisescelis21@gmail.com', '$2y$10$Ob9iRVKPw.DiqPASkyUibOERwWkE7PMQaSsmqkDFYc5iLvXdJqXle', 0, 1, '2025-10-05 19:52:49', 'd7518474-2d2f-4634-823f-71936565c110', NULL, NULL, NULL, NULL),
 ('35', 'Ricardo', 'jose.2710.ricardo@gmail.com', '$2y$10$xTVUq4JFTu6S7z7pCZtWTuC8ukfj0r3jsFk7Nw62TtU1WtnZ87MDm', 1, 1, NULL, NULL, '2025-10-04 10:49:45', '35', NULL, NULL),
 ('40', 'Hilson Martinez', 'martinezhilson8@gmail.com', '$2y$10$3mEuUd1/uIn0nNx3.qBoYeGeDc7WAXsEUvldqHX1WNaWusgVwnu9e', 2, 1, NULL, NULL, NULL, NULL, '2025-10-05 16:14:03', 'd7518474-2d2f-4634-823f-71936565c110'),
 ('42', 'ASDRUBAL MARTINEZs', 'asdrubalmartinez486@gmail.com', '$2y$10$yUnVJhDWX6xkB4BEch2HPeAbEGNA311qcjs1DXVIsTmaah6jzHwzW', 2, 1, NULL, NULL, '2025-10-04 09:49:28', '42', '2025-10-05 16:13:58', 'd7518474-2d2f-4634-823f-71936565c110'),
@@ -599,6 +658,13 @@ ALTER TABLE `periodos_servicio`
   ADD KEY `fk_ps_verraco` (`verraco_id`);
 
 --
+-- Indices de la tabla `recintos`
+--
+ALTER TABLE `recintos`
+  ADD PRIMARY KEY (`recinto_id`),
+  ADD UNIQUE KEY `uq_area_codigo_recinto` (`area_id`,`codigo_recinto`);
+
+--
 -- Indices de la tabla `reportes_dano`
 --
 ALTER TABLE `reportes_dano`
@@ -713,6 +779,12 @@ ALTER TABLE `partos`
 ALTER TABLE `periodos_servicio`
   ADD CONSTRAINT `fk_ps_hembra` FOREIGN KEY (`hembra_id`) REFERENCES `animales` (`animal_id`) ON UPDATE CASCADE,
   ADD CONSTRAINT `fk_ps_verraco` FOREIGN KEY (`verraco_id`) REFERENCES `animales` (`animal_id`) ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `recintos`
+--
+ALTER TABLE `recintos`
+  ADD CONSTRAINT `fk_recinto_area` FOREIGN KEY (`area_id`) REFERENCES `areas` (`area_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Filtros para la tabla `reportes_dano`
